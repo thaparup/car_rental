71d721a122f28a1202acb998d583bfebddaf3023
thaparup <thaparup3965@gmail.com>
2024-01-26 20:14:11 +0545

    Hero Section media query completed

--- src/componenets/HeroSection.jsx
+++ src/componenets/HeroSection.jsx
@@ -6,50 +6,41 @@ import MainCar from "../assets/hero/main-car.png";
 
 export default function HeroSection() {
   return (
-    <section className="bg-bg ">
-      <Navbar />
-
-      <div className="absolute top-0 right-0 ">
-        <img src={BgShape} alt="" className="" />
-      </div>
-
-      <div className="flex py-16 mx-[7.2rem] ">
-        <div className=" relative pt-16 pl-2 w-[42%]">
-          <h4 className="text-[1.5rem] font-bold text-pmBlack font-rubik pb-[0.4rem]">
+    <section className="bg-bg  relative h-[97vh] px-[6rem] lg:px-[5rem] md:px-[4rem] sm:px-[3rem] xsm:px-[1rem] overflow-hidden">
+      <img src={BgShape} alt="" className="absolute top-0 right-0 xsm:hidden" />
+      <div className="flex justify-between mx-auto py-[10.4rem] items-center mt-28">
+        <div className="  relative z-40  w-[32rem]  sm:mx-auto xsm:mx-auto xsm:w-[90%] ">
+          <h4 className="text-[1.4rem] font-bold text-pmBlack font-rubik pb-[0.4rem] xsm:text-center">
             Plan your trip now
           </h4>
-          <h1 className="text-pmBlack text-[3.3rem] font-bold font-poppins leading-[4rem]">
+          <h1 className="text-pmBlack text-[3.3rem] xsm:text-[3rem] lg:text-[3rem] font-bold font-poppins leading-[4rem] xsm:text-center">
             Save <span className="text-pmColor">big</span> with our car rental
           </h1>
 
-          <p className="py-5 text-gray-700 ">
+          <p className="py-5 text-gray-700 text-[0.96rem] max-w-[85%]  xsm:text-center">
             Rent the car of your dreams. Unbeatable prices, unlimited miles,
             flexible pick-up options and much more.
           </p>
-          <div className="flex gap-5 pt-5">
+          <div className="flex gap-5 pt-5 xsm:flex-col xsm:gap-4 xsm:pt-0">
             <a
               href=""
-              className="flex bg-pmColor px-8 py-4 rounded-sm font-bold text-white shadow-boxShadowRed transitons-shadow duration-300 hover:shadow-boxShadowRedHover "
-              to="/"
+              className="text-white bg-pmColor py-5 px-8 flex gap-3 rounded-sm font-bold transition-all duration-300 ease-in-out hover:shadow-boxShadowRed xsm:mx-auto"
             >
-              <div className="flex gap-2 mt-1">
-                <p>Book Ride</p>
-
-                <IconCircleCheck />
-              </div>
+              <p>Book Ride</p>
+              <IconCircleCheck />
             </a>
-            <a
-              href="/"
-              className="text-white bg-pmBlack py-5 px-8 flex rounded-sm font-bold shadow-boxShadow"
-            >
-              Learn More &nbsp;{" "}
+            <div className="text-white bg-pmBlack py-5 px-8 flex rounded-sm font-bold shadow-boxShadow xsm:mx-auto transition-colors duration-500 ease-out hover:bg-white hover:border-[1px] border-pmBlack hover:text-pmBlack">
+              <p>Learn More</p>
               <IconChevronRight className="translate-y-[1px]" />
-            </a>
+            </div>
           </div>
         </div>
-        <div className="relative z-10 ">
-          <img src={MainCar} alt="" className="" />
-        </div>
+
+        <img
+          src={MainCar}
+          alt=""
+          className="absolute z-20 right-0 w-[65%] sm:hidden xsm:hidden "
+        />
       </div>
     </section>
   );
