786d138478415c48aa7c3a708cb1d4d4810bca91
thaparup <thaparup3965@gmail.com>
2024-01-30 11:32:46 +0545

    css of bg of download section image

--- src/componenets/Download.jsx
+++ src/componenets/Download.jsx
@@ -4,7 +4,7 @@ import Img2 from "../assets/download/googleapp.svg";
 
 export default function Download() {
   return (
-    <section className="py-[8rem]  px-[7.2rem] lg:px-[6.2rem] md:px-[5.2rem] sm:px-[3.8rem] xsm:px-[1.4rem] bg-[url('src/assets/banners/bg02.png')]  h-auto bg-no-repeat bg-cover bg-[#f8f8f8]">
+    <section className="py-[8rem]  px-[7.2rem] lg:px-[6.2rem] md:px-[5.2rem] sm:px-[3.8rem] xsm:px-[1.4rem] download h-auto bg-no-repeat bg-cover bg-[#f8f8f8]">
       <div className="flex flex-col gap-6 max-w-[35rem] ">
         <h2 className="text-[2.7rem] font-bold font-poppins md:text-center sm:text-center xsm:text-center">
           Download our app to get most out of it
